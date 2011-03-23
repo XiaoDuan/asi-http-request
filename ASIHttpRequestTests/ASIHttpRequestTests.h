0106cfe147c3fa38d36b0252bf4b401e2d0f0f3b
Ray Yamamoto Hilton <ray@wirestorm.net>
2011-03-23 14:47:36 +1100

    Add scaffold config and add static library

---
+++ ASIHttpRequestTests/ASIHttpRequestTests.h
@@ -0,0 +1,17 @@
+//
+//  ASIHttpRequestTests.h
+//  ASIHttpRequestTests
+//
+//  Created by Ray Hilton on 23/03/11.
+//  Copyright 2011 Sensis. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+
+@interface ASIHttpRequestTests : SenTestCase {
+@private
+    
+}
+
+@end
---
+++ ASIHttpRequestTests/ASIHttpRequestTests.m
@@ -0,0 +1,33 @@
+//
+//  ASIHttpRequestTests.m
+//  ASIHttpRequestTests
+//
+//  Created by Ray Hilton on 23/03/11.
+//  Copyright 2011 Sensis. All rights reserved.
+//
+
+#import "ASIHttpRequestTests.h"
+
+
+@implementation ASIHttpRequestTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in ASIHttpRequestTests");
+}
+
+@end
